1b39bc6b1a844d8d8bffe9725a144843880388a0
KorzJ <511722164@qq.com>
2016-12-30 13:45:56 +0800

    Initial Commit

---
+++ LK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LK
+//
+//  Created by KorzJ on 2016/12/30.
+//  Copyright © 2016年 KorzJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LK
+//
+//  Created by KorzJ on 2016/12/30.
+//  Copyright © 2016年 KorzJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
